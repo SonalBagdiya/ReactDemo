e8a30add2fb8b13a2d6eb160417f64b6ceabb04a
Sonal Bagadia <sonalb@synerzipune.local>
2016-07-29 17:41:59 +0530

    change backend url

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,6 @@
 let config = {
-    BASE_URL: "http://localhost:3000"
-    //BASE_URL: "https://billing-server.herokuapp.com"
+    //BASE_URL: "http://localhost:3000"
+    BASE_URL: "https://billing-server.herokuapp.com"
 };
 
 export default config;
