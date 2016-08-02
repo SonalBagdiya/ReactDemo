fd05b7c789a0d7b8d1b6511935ae057999f16df4
Sonal Bagadia <sonalb@synerzipune.local>
2016-08-02 20:07:56 +0530

    dist folder commit

--- src/js/config.js
+++
@@ -1,7 +0,0 @@
-let config = {
-    //BASE_URL: "http://localhost:3000"
-    BASE_URL: "https://billing-server.herokuapp.com"
-};
-
-export default config;
-
--- src/js/reducers/common.js
+++
@@ -1,7 +0,0 @@
-import {createReducer} from "../utils";
-
-const initialState = {
-    paginationLimit: 10,
-};
-
-export default createReducer(initialState, {});
